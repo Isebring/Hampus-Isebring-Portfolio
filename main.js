90d82fda544c6f86036f5b8d5324fcc147f6d647
Hampus Isebring <hampus.isebring@gmail.com>
2022-11-24 11:08:23 +0100

    added my first JS interactions

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 window.addEventListener('DOMContentLoaded', main);
+
+function main() {
+  hamburgerMenuAnimation();
+  reRunTypewriter();
+  }
+
+
+function hamburgerMenuAnimation() {
+  let menu = document.querySelector('.hamburger-menu-icon');
+  
+  menu.onclick = () => {
+    menu.classList.toggle("move");
+  };
+  }
+
+  function logoReturnHome() {
+    location.href= 'homepage.html';
+  }
+
+  function reRunTypewriter() {
+  let goAgain; 
+  goAgain = setInterval(reRun, 5000);
+
+  function reRun() {
+    typeWriter = document.querySelector('#typewriter');
+    typeWriter.goAgain;
+  }
+  }
