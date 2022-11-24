2f426dc4ea0adf7bc0a047cc94389b6562d95666
Hampus Isebring <hampus.isebring@gmail.com>
2022-11-24 16:44:07 +0100

    Added JS for swiper slider + tried out a back top top

--- main.js
+++ main.js
@@ -1,29 +1,79 @@
-window.addEventListener('DOMContentLoaded', main);
+window.addEventListener("DOMContentLoaded", main);
 
 function main() {
   hamburgerMenuAnimation();
-  reRunTypewriter();
-  }
-
+  topFunction();
+  scrollFunction();
+  SwiperSlider();
+}
 
 function hamburgerMenuAnimation() {
-  let menu = document.querySelector('.hamburger-menu-icon');
-  
+  let menu = document.querySelector(".hamburger-menu-icon");
+
   menu.onclick = () => {
     menu.classList.toggle("move");
   };
-  }
+}
 
-  function logoReturnHome() {
-    location.href= 'homepage.html';
-  }
+function logoReturnHome() {
+  location.href = "homepage.html";
+}
 
-  function reRunTypewriter() {
-  let goAgain; 
-  goAgain = setInterval(reRun, 5000);
+window.onscroll = function () {
+  scrollFunction();
+};
 
-  function reRun() {
-    typeWriter = document.querySelector('#typewriter');
-    typeWriter.goAgain;
+function scrollFunction() {
+  let toTopButton = document.getElementById("myBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    toTopButton.style.display = "block";
+  } else {
+    toTopButton.style.display = "none";
   }
-  }
+}
+
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+  console.log("hej");
+}
+
+function SwiperSlider() {
+  const swiper = new Swiper(".swiper", {
+    effect: 'creative',
+    creativeEffect: {
+     prev: {
+      translate: [0, 0, -400],
+     },
+     next: {
+      translate: ['100%', 0, 0],
+     },
+    },
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,  
+    },
+    direction: "horizontal",
+    loop: true,
+
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
+
+// function reRunTypewriter() {
+//   typeWriter = document.querySelector("#typewriter");
+//   const index = 0;
+//   const text = "I am a junior UI...";
+//   setInterval(typeChar, 800);
+
+//   function typeChar() {
+//     typeWriter.textContent += text[index];
+//     index++;
+//   }
+// }
