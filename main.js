014a65c6c29104463847abdbc73d75ec4dfd0ed9
Hampus Isebring <hampus.isebring@gmail.com>
2022-12-02 00:05:42 +0100

    Cleaned up. Added some JSDoc

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 window.addEventListener("DOMContentLoaded", main);
-
+/** Here marks the start of the page, the main function which loads after the DOM content is loaded */
 function main() {
   addEventListeners();
   SwiperSlider();
 }
-
+/** This function serves as the first thing the main function loads after the DOM content is loaded, which is the event listeners */
 function addEventListeners() {
   headerBackgroundChange();
   arrowBacktoTop();
-  // headerListColorChange();
   hamburgerMenuAnimationAndSidebarToggle();
   removeSidebarOnScroll();
 }
-
+/**
+ * This function is imported from the Swiper.js library and serves as the slider/carousel containing images
+ */
 function SwiperSlider() {
+  if(this.Swiper === undefined) {return};
   const swiper = new Swiper(".swiper", {
     effect: "creative",
     creativeEffect: {
@@ -25,7 +27,7 @@ function SwiperSlider() {
       },
     },
     autoplay: {
-      delay: 4500,
+      delay: 5000,
       disableOnInteraction: false,
     },
     direction: "horizontal",
@@ -41,7 +43,9 @@ function SwiperSlider() {
     },
   });
 }
-
+/**
+ * This function serves as the toggle and removal of the sidebar with the hamburger-menu icon. The icon appears on screen sizes 775px and smaller.
+ */
 function hamburgerMenuAnimationAndSidebarToggle() {
   let menu = document.querySelector(".hamburger-menu-icon");
   let navbar = document.querySelector(".navbar");
@@ -51,31 +55,22 @@ function hamburgerMenuAnimationAndSidebarToggle() {
     menu.classList.toggle("move");
   };
 }
-
+/** The purpose of this function is to close the sidebar if the user starts to scroll on the page */
 function removeSidebarOnScroll() {
   let menu = document.querySelector(".hamburger-menu-icon");
   let navbar = document.querySelector(".navbar");
-window.onscroll = () => {
-  navbar.classList.remove("toggle-sidebar");
-  menu.classList.remove("move");
-};
+  window.onscroll = () => {
+    navbar.classList.remove("toggle-sidebar");
+    menu.classList.remove("move");
+  };
 }
 
-// function openSidebar() {
-// let navbar = document.querySelector(".navbar");
-
-// navbar.onclick = () => {
-// navbar.classList.toggle("open-sidebar");
-// };
-
-// }
-
-
-
+/** This simple function enables the logo to be clickable and return the user to the top of the page */
 function logoReturnHome() {
   location.href = "#home";
 }
 
+/** This function enables a background color instead of the transparent one for the header when the user scrolls. This to avoid the elements contained in the header from colliding with the elements within the page */
 function headerBackgroundChange() {
   let header = document.querySelector("header");
   window.addEventListener("scroll", () => {
@@ -83,47 +78,10 @@ function headerBackgroundChange() {
   });
 }
 
-// function headerListColorChange() {
-//   let colorChange = document.querySelector(".nav-link");
-//  window.addEventListener("scroll", () => {
-//   colorChange.classList.toggle("list-active", window.scrollY > 0);
-//   console.log('yoyooyo');
-//  });
-// }
-
+/** This function provides another way for the user to get back to the top of the page. This code also make the arrow pop up after the user have scrolled down a bit */
 function arrowBacktoTop() {
   let arrowScrollTop = document.querySelector(".scroll-back-to-top");
   window.addEventListener("scroll", () => {
     arrowScrollTop.classList.toggle("scroll-active", window.scrollY >= 400);
   });
 }
-
-// window.onscroll = function () {
-//   scrollFunction();
-// };
-
-// function scrollFunction() {
-//   let toTopButton = document.getElementById("myBtn");
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     toTopButton.style.display = "block";
-//   } else {
-//     toTopButton.style.display = "none";
-//   }
-// }
-
-// function topFunction() {
-//   document.documentElement.scrollTop = 0;
-//   console.log("hej");
-// }
-
-// function reRunTypewriter() {
-//   typeWriter = document.querySelector("#typewriter");
-//   const index = 0;
-//   const text = "I am a junior UI...";
-//   setInterval(typeChar, 800);
-
-//   function typeChar() {
-//     typeWriter.textContent += text[index];
-//     index++;
-//   }
-// }
